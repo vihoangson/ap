0736831290a74683588a1c115f863dfacbebf85c
Vi Hoang Son <vihoangson@gmail.com>
2022-10-11 17:18:27 +0700

    second commit

--- resources/views/ap.blade.php
+++ resources/views/ap.blade.php
@@ -1,64 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Peaceful Place</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-    </head>
-
-    <body>
-
- <!-- page-header -->
-    <div class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="page-caption">
-                        <h1 class="page-title">Peaceful Place</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- /.page-header-->
-    <!-- news -->
-    <div class="card-section">
-        <div class="container">
-            <div class="card-block bg-white mb30">
-                <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <!-- section-title -->
-                        <div class="section-title mb-0">
-                            <h2>Days together: {{$days}} </p>
-                        </div>
-                        <!-- /.section-title -->
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">Created for <a href="/" target="_blank">Peaceful Place</a>
-              </div></div>
-            </div>
-            </div>
-            </div>
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-
+@extends('layouts.app1')
+@section('BodyContent')
+<h2>Days together: {{$days}} </p>
+@endsection
+@section('FooterContent')
     <script>
-        setInterval(()=>{
-            location.reload();
-        }, 5000);
+        var i = 0;
+        setInterval(()=>{            
+            if(i > 100){
+                i = 0;
+                $(".progress").hide();                
+                $(".page-header,.card-section").hide();
+                $(".loading-page").show();
+                setInterval(()=>{    
+                    location.reload();                
+                },2000);
+                
+            }else{
+                $(".progress-bar").css({"width":i+"%"});
+                i+=1
+                console.log(i);
+            }            
+        }, 100);
     </script>
-    </body>
-</html>
+@endsection
+    
--- resources/views/layouts/app1.blade.php
+++ resources/views/layouts/app1.blade.php
@@ -1,81 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="/favicon.ico">
-    <title>Tiết Kiệm Tiền</title>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <!-- Custom styles for this template -->
-    @yield('headerContent')
-    <link href="grid.css" rel="stylesheet">
-</head>
-<body>
+    
 
-<div class="container">
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link" href="/">Home page</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/chart">Chart</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/quiz">Quiz</a>
-        </li>
-    </ul>
-    @yield('content')
-    <hr>
-    <div class="card-group d-none">
-        <div class="card">
-            <img class="card-img-top" src="https://via.placeholder.com/400x200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This content is a little bit longer.</p>
-            </div>
-            <div class="card-footer">
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="https://via.placeholder.com/400x200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="https://via.placeholder.com/400x200" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                    content. This card has even longer content than the first to show that equal height action.</p>
-            </div>
-            <div class="card-footer">
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <title>Peaceful Place</title>
+
+    <!-- Bootstrap core CSS -->
+   <link rel="stylesheet" href="/init/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+    </head>
+
+    <body>
+<div class="progress" style=' height:3px;'>
+  <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+ <!-- page-header -->
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="page-caption">
+                        <h1 class="page-title">Peaceful Place</h1>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div> <!-- /container -->
-<!-- Bootstrap core JavaScript
-    ================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script
-    src="https://code.jquery.com/jquery-3.6.1.min.js"
-    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-@yield('footerContent')
-</body>
+    <!-- /.page-header-->
+    <!-- news -->
+    <div class="card-section">
+        <div class="container">
+            <div class="card-block bg-white mb30">
+                <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <!-- section-title -->
+                        <div class="section-title mb-0">
+                            @yield('BodyContent')
+                            
+                        </div>
+                        <!-- /.section-title -->
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">Created for <a href="/" target="_blank">Peaceful Place</a>
+              </div></div>
+            </div>
+            </div>
+            </div>
+                    <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/init/jquery-3.5.1.slim.min.js"></script>
+        <script src="/init/popper.min.js"></script>
+        <script src="/init/bootstrap.min.js" ></script>
+        @yield('FooterContent')
+
+        <div class="loading-page"></div>
+    </body>
 </html>
